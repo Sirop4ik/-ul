f5b6f9fe317ee84994e528eb263a7cf99cf630ef
Sirop4ik <67153044+Sirop4ik@users.noreply.github.com>
2020-06-19 16:51:46 +0300

    Add files via upload

---
+++ Учебная практика/УП1/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(385, 220);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.AllowUserToResizeRows = false;
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.ColumnHeadersVisible = false;
+            this.dataGridView2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView2.EnableHeadersVisualStyles = false;
+            this.dataGridView2.Location = new System.Drawing.Point(474, 64);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(380, 220);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.AllowUserToResizeRows = false;
+            this.dataGridView3.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.ColumnHeadersVisible = false;
+            this.dataGridView3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView3.EnableHeadersVisualStyles = false;
+            this.dataGridView3.Location = new System.Drawing.Point(238, 385);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersVisible = false;
+            this.dataGridView3.RowTemplate.ReadOnly = true;
+            this.dataGridView3.Size = new System.Drawing.Size(410, 216);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(226, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 22);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Создать матрицу";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gainsboro;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(686, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 22);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Создать матрицу";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Location = new System.Drawing.Point(13, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox2.Location = new System.Drawing.Point(120, 38);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox3.Location = new System.Drawing.Point(580, 38);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox4.Location = new System.Drawing.Point(474, 38);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gainsboro;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(398, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Сложить";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gainsboro;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(398, 157);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Умножить";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Gainsboro;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(12, 290);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(166, 24);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Транспонировать";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Gainsboro;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(691, 290);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(166, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Транспонировать";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(383, 355);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 26);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Результат";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 26);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Матрица А";
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Gainsboro;
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(336, 605);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(214, 23);
+            this.button9.TabIndex = 19;
+            this.button9.Text = "Начать сначала";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(735, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 26);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Матрица В";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(869, 656);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Калькулятор матриц";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Учебная практика/УП1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            for (int i = 0; i < b; i++)
+            {
+                dataGridView1.Columns.Add("", "");
+            }
+            for (int j = 0; j < a; j++)
+            {
+                dataGridView1.Rows.Add();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox4.Text);
+            int b = int.Parse(textBox3.Text);
+            for (int i = 0; i < b; i++)
+            {
+                dataGridView2.Columns.Add("", "");
+            }
+            for (int j = 0; j < a; j++)
+            {
+                dataGridView2.Rows.Add();
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int t = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            for (int i = 0; i < y; i++)
+            {
+                dataGridView3.Columns.Add("", "");
+            }
+            for (int j = 0; j < t; j++)
+            {
+                dataGridView3.Rows.Add();
+            }
+            int n = dataGridView1.ColumnCount;
+            int m = dataGridView1.RowCount;
+            int q = dataGridView2.ColumnCount;
+            int r = dataGridView2.RowCount;
+            int[,] a = new int[n, m];
+            int[,] b = new int[n, m];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    a[i, j] = Convert.ToInt32(dataGridView1[i, j].Value);
+            for (int i = 0; i < q; i++)
+                for (int j = 0; j < r; j++)
+                    b[i, j] = Convert.ToInt32(dataGridView2[i, j].Value);
+            for (int i = 0; i < dataGridView3.Rows.Count; i++)
+                for (int j = 0; j < dataGridView3.Columns.Count; j++)
+                    dataGridView3[i, j].Value = a[i, j] + b[i, j];
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == textBox4.Text)
+            {
+                dataGridView3.Rows.Clear();
+                dataGridView3.Columns.Clear();
+                int[,] A = new int[dataGridView1.RowCount, dataGridView1.ColumnCount];
+                int[,] B = new int[dataGridView2.RowCount, dataGridView2.ColumnCount];
+                for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                    for (int j = 0; j < dataGridView1.RowCount; j++)
+                        A[j, i] = Convert.ToInt32(dataGridView1[i, j].Value);
+                for (int i = 0; i < dataGridView2.ColumnCount; i++)
+                    for (int j = 0; j < dataGridView2.RowCount; j++)
+                        B[j, i] = Convert.ToInt32(dataGridView2[i, j].Value);
+
+                for (int i = 0; i < dataGridView2.ColumnCount; i++)
+                {
+                    dataGridView3.Columns.Add("", "");
+                }
+                for (int j = 0; j < dataGridView1.RowCount; j++)
+                {
+                    dataGridView3.Rows.Add();
+                }
+
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    for (int j = 0; j < dataGridView2.ColumnCount; j++)
+                    {
+                        int s = 0;
+                        for (int k = 0; k < dataGridView2.RowCount; k++)
+                            s += A[i, k] * B[k, j];
+                        dataGridView3[j, i].Value = s;
+                    }
+                }
+            }
+            else MessageBox.Show("Число столбцов первой матрицы не соответствует числу строк во второй", "Ошибка");         
+        }
+        public int MultyMatrix(int A, int B)
+        {
+          int C;
+          C = A * B;
+          return C;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int t = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            for (int i = 0; i < y; i++)
+            {
+                dataGridView3.Columns.Add("", "");
+            }
+            for (int j = 0; j < t; j++)
+            {
+                dataGridView3.Rows.Add();
+            }
+            int n = dataGridView1.ColumnCount;
+            int m = dataGridView1.RowCount;
+            int[,] a = new int[n, m];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    a[i, j] = Convert.ToInt32(dataGridView1[i, j].Value);
+            for (int i = 0; i < dataGridView3.Rows.Count; i++)
+                for (int j = 0; j < dataGridView3.Columns.Count; j++)
+                    dataGridView3[i, j].Value = a[j, i];
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int t = int.Parse(textBox3.Text);
+            int y = int.Parse(textBox4.Text);
+            for (int i = 0; i < y; i++)
+            {
+                dataGridView3.Columns.Add("", "");
+            }
+            for (int j = 0; j < t; j++)
+            {
+                dataGridView3.Rows.Add();
+            }
+            int n = dataGridView2.ColumnCount;
+            int m = dataGridView2.RowCount;
+            int[,] b = new int[n, m];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    b[i, j] = Convert.ToInt32(dataGridView2[i, j].Value);
+            for (int i = 0; i < dataGridView3.Rows.Count; i++)
+                for (int j = 0; j < dataGridView3.Columns.Count; j++)
+                    dataGridView3[i, j].Value = b[j, i];
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView3.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView2.Columns.Clear();
+            dataGridView3.Columns.Clear();
+        }
+    }
+}
